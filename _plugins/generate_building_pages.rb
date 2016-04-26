216fa426e899230473c4aaa9ccde0bd3a5e4e023
seb <seb.bacon@gmail.com>
2016-04-26 19:02:27 +0100

    Initial buildings support

---
+++ _plugins/generate_building_pages.rb
@@ -0,0 +1,31 @@
+module Jekyll
+  class BuildingPage < Page
+    def initialize(site, base, dir, building)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = "#{building['uid']}.html"
+
+      # Extract info from the filename
+      self.process(@name)
+
+      # Read YAML frontmatter and template and load into data structure
+      self.read_yaml(File.join(base, '_layouts'), 'building.html')
+      self.data['title'] = "Building #{building['uid']}"
+      self.data['building'] = building
+    end
+  end
+
+  class BuildingPageGenerator < Generator
+    safe true
+
+    def generate(site)
+      dir = 'buildings'
+      site.data['buildings'].each do |building|
+        site.pages << BuildingPage.new(
+          site, site.source, 'buildings', building)
+      end
+    end
+  end
+
+end
